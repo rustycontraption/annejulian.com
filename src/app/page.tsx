27fb9f6014d6cf69103ac999f3e62f1be6d16cb9
Anne Julian <julian@allenai.org>
2025-05-30 15:29:57 -0700

    Add project tiles for all projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,9 @@ import classes from "../components/ProjectTile.module.css";
 const projectTiles = [
   { link: '/rabbit', tileImg: "https://static.annejulian.net/static/img/rabbit/rabbit.png", tileText: "lorem ipsum dolor sit amet", key: 'rabbit' },
   { link: '/miniped', tileImg: "https://static.annejulian.net/static/img/miniped/mastminiped.jpg", tileText: "lorem ipsum dolor sit amet", key: 'miniped' },
-  { link: '/dive', tileImg: "https://static.annejulian.net/static/img/dive/mastdive.jpg", tileText: "lorem ipsum dolor sit amet", key: 'dive' }
+  { link: '/dive', tileImg: "https://static.annejulian.net/static/img/dive/mastdive.jpg", tileText: "lorem ipsum dolor sit amet", key: 'dive' },
+  { link: '/drone', tileImg: "https://static.annejulian.net/static/img/drone/drone_hero.png", tileText: "lorem ipsum dolor sit amet", key: 'drone' },
+  { link: '/gs750', tileImg: "https://static.annejulian.net/static/img/gs750/gs750_hero.png", tileText: "lorem ipsum dolor sit amet", key: 'gs750' }
 ]
 
 // const [active, setActive] = useState(projectTiles[0].link);
