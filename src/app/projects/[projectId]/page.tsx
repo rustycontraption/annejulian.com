5aa6deb9b8ef70b1ad0fdb3c5a1993fe83815413
Anne Julian <julian@allenai.org>
2025-06-08 15:06:27 -0700

    Modify the Hero component to support optional video headers

---
+++ src/app/projects/[projectId]/page.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import { useParams } from 'next/navigation'
+import { Hero } from "../../../components/Hero"
+import ProjectTimeline from "../../../components/ProjectTimeline"
+import { projectContent } from "../../../resources/content"
+import { Container } from "@mantine/core"
+import classes from "../../../components/Page.module.css"
+
+
+export default function Projects() {
+    const params = useParams()
+    const currentProject = params.projectId as string;
+    const currentProjectContent = projectContent[currentProject];
+
+    const videoHeaderProjects = ["drone", "windshadow"]
+    const isVideoHeader = videoHeaderProjects.includes(currentProject)
+        ? true
+        : false
+
+    return (
+        <div>
+            <Hero currentProject={currentProject} isVideoHeader={isVideoHeader} />
+            <div style={{ height: 50 }}></div>
+            <Container className={classes.contentTimeline} size="xl">
+                <ProjectTimeline logEntries={currentProjectContent} />
+            </Container>
+        </div>
+    )
+}
