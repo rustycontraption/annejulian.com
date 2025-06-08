c9540f5cec5805ddb34ce665bed134d48f3f1e2b
Anne Julian <julian@allenai.org>
2025-06-08 15:07:43 -0700

    Enable web links for the ProjectTile component

--- src/components/ProjectTile.tsx
+++ src/components/ProjectTile.tsx
@@ -1,16 +1,21 @@
 import { Title } from "@mantine/core";
+import Link from "next/link";
 import classes from "../components/ProjectTile.module.css";
 
 interface ProjectTileProps {
     tileImg: string,
-    tileText: string
+    tileText: string,
+    href: string
 }
 
-export default function ProjectTile({ tileImg, tileText }: ProjectTileProps) {
+export default function ProjectTile({ tileImg, tileText, href }: ProjectTileProps) {
     return (
+
         <div className={classes.tileContainer}>
-            <img src={tileImg} className={classes.tileImg} />
-            <Title className={classes.tileText}>{tileText}</Title>
-        </div>
+            <Link href={href}>
+                <img src={tileImg} className={classes.tileImg} />
+                <Title className={classes.tileText}>{tileText}</Title>
+            </Link>
+        </div >
     )
 }
