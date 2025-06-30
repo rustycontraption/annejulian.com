eb6ce6756ca5ce131a1fb24494bd14f78ee8719d
Annie <rustycontraption@gmail.com>
2025-06-29 22:09:51 -0700

    Implement mailto in SocialLinks

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -6,7 +6,7 @@ export default function SocialLinks() {
         <Group gap={20} visibleFrom="xs">
             <a href="https://github.com/rustycontraption/annejulian.com"><FaGithub size={22} /></a>
             <a href="https://www.linkedin.com/in/annejulian/"><FaLinkedin size={22} /></a>
-            {/* <a href={`mailto:${process.env.NEXT_PUBLIC_EMAIL}`}><FaEnvelope size={22} /></a> */}
+            <a href={`mailto:${process.env.NEXT_PUBLIC_EMAIL}`}><FaEnvelope size={22} /></a>
         </Group>
     )
 }
