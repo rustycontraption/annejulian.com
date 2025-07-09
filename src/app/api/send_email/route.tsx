15662fc3d1d0ef1b93358fbfe46b77442e76cea9
Annie <rustycontraption@gmail.com>
2025-07-09 11:45:05 -0700

    Try not specifying aws creds so amplify uses the attached roles

--- src/app/api/send_email/route.tsx
+++ src/app/api/send_email/route.tsx
@@ -3,10 +3,10 @@ import { SNSClient, PublishCommand } from '@aws-sdk/client-sns';
 
 const snsClient = new SNSClient({
     region: 'us-west-2',
-    credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-    },
+    // credentials: {
+    //     accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    //     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    // },
 });
 
 export async function POST(req: NextRequest) {
